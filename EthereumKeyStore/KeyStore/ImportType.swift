b461903b83473abe2b63391f3bff588ee21cf63f
alex <ioswpf@gmail.com>
2018-05-03 18:24:42 +0800

    提交

--- EthereumKeyStore/KeyStore/ImportType.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum ImportType {
-    case keystore(string: String, password: String)
-    case privateKey(privateKey: String, password: String)
-    case mnemonic(words: [String], password: String)
-    case watch(address: Address)
-}
-
